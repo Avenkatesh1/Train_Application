eb2076ef72164a23bf9175a1c886bb00061bd682
A Venkatesh <avenkateshanand1998@gmail.com>
2024-07-05 13:04:04 +0530

    create routes here

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { SearchComponent } from './pages/search/search.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:"",
+        redirectTo:"Home",
+        pathMatch:"full"
+    },
+    {
+        path:"Home",
+        component:HomeComponent
+    },
+    {
+        path:"Search/:formStationId/:toStationId/:dateOfTravel",
+        component:SearchComponent
+    }
+];
