34b3e1651fca24d6a72bdf982ef56241faf36f61
A Venkatesh <avenkateshanand1998@gmail.com>
2024-07-06 12:24:26 +0530

    i have implementing the some of the components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { SearchComponent } from './pages/search/search.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
 
 export const routes: Routes = [
     {
@@ -15,5 +17,13 @@ export const routes: Routes = [
     {
         path:"Search/:formStationId/:toStationId/:dateOfTravel",
         component:SearchComponent
+    },
+    {
+        path:"Login",
+        component:LoginComponent
+    },
+    {
+        path:"Register",
+        component:RegisterComponent
     }
 ];
